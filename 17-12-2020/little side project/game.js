f0e4ec9bb227f8d6839e7709c13dfc6013b67a18
Leesinbaka <34050544+Leesinbaka@users.noreply.github.com>
2020-12-23 11:19:05 +0800

    better way to design a new draw object

--- 17-12-2020/little side project/game.js
+++ 17-12-2020/little side project/game.js
@@ -2,25 +2,34 @@
     <body onload="draw();  ">  
     <canvas id="canvas" width="510" height="510"> */}  
 
-function draw(){  
+function draw(){
     
-    var ctx = document.getElementById('canvas').getContext('2d');    
+    var ctx = document.getElementById('canvas').getContext('2d');
 
-    ctx.clearRect(0,0,510,510);    
+    ctx.clearRect(0,0,510,510);
 
     //block
-    ctx.save();    
-    ctx.fillSytle = '#FF000' ;  
-    var time = new Date();  
-    ctx.fillRect(time.getSeconds()*3,time.getSeconds()*3, 20, 20);  
-    
+    ctx.save();
+    var time = new Date();
+    var block ={
+        x : time.getSeconds()*3,
+        y : time.getSeconds()*3,
+        height : 20,
+        width : 20,
+        color : 'red',
+        draw : function(){
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }   
+    };
+    block.draw();
     //bound map
-    ctx.beginPath();  
-    ctx.lineWidth = "2";  
-    ctx.strokeStyle = "black";  
-    ctx.rect(5, 5, 500, 500);  
-    ctx.stroke();  
+    ctx.beginPath();
+    ctx.lineWidth = "1";
+    ctx.strokeStyle = "black";
+    ctx.rect(5, 5, 500, 500);
+    ctx.stroke();
     
-    ctx.restore();  
-    window.requestAnimationFrame(draw);  
+    ctx.restore();
+    window.requestAnimationFrame(draw);
 }
