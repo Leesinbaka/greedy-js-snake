71949a9521877bab50f8d1aebd267842ad052c09
Leesinbaka <34050544+Leesinbaka@users.noreply.github.com>
2020-12-31 12:51:31 +0800

    move ya but insane move

--- 17-12-2020/little side project/game.js
+++ 17-12-2020/little side project/game.js
@@ -1,15 +1,42 @@
+var blockx = 250;
+var blocky = 250;
+var deltax = 0;
+var deltay = 0;
 function draw(){  
     var cas = document.getElementById('canvas');   
     var ctx = cas.getContext('2d');
     ctx.clearRect(0,0,510,510);    
 
     //block  
-    function movingblock(){
-        var width = 20
-        var height = 20
-        window.addeventlistener(),
-        draw : function(){}
+   
+   window.addEventListener('keydown',function movement(e){
+    document.getElementById('keypressed').innerHTML = e.key;
+    switch(e.key){
+        case 'w':
+            deltay -=2 ;
+            break;
+        case 's':
+            deltay +=2 ;
+            break;
+        case 'a':
+            deltax -=2 ;
+            break;
+        case 'd':
+            deltax +=2 ;
+            break;
     }
+    
+    
+
+   });
+   function block(){
+    ctx.clearRect(0,0,cas.height,cas.width);  
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.fillRect(blockx + deltax,blocky + deltay,20,20);
+    ctx.closePath();
+}
+block();
     //bound map
     var boundmap = {
         lineWidth : "2",
